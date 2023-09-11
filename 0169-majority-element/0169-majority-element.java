6936aca8e5ef5c697eb36e866f1e854bf3ec2b1d
BENDU MANOHAR NAIDU <45328696+bendumanoharnaidu2001@users.noreply.github.com>
2023-09-11 22:53:27 +0530

    Time: 1 ms (99.85%), Space: 48.8 MB (22.89%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int element = 0;
+        int freq = 0;
+        for (int i=0; i<nums.length; i++) {
+            if (freq==0) {
+                element = nums[i];
+                freq++;
+                continue;
+            }
+            if (nums[i] == element) {
+                freq++;
+            }
+            else {
+                freq--;
+            }
+        }
+        return element;
+    }
+}
