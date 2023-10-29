a145f2ce8e7f041d4737174e906054d2dfd3feb7
BENDU MANOHAR NAIDU <45328696+bendumanoharnaidu2001@users.noreply.github.com>
2023-10-29 18:26:14 +0530

    Time: 0 ms (100%), Space: 44.2 MB (65.4%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,46 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int result [] = new int[2];
-        int lb = lower_bound(nums, nums.length, target);
-        if (lb==nums.length || nums[lb]!=target) {
-            result[0] = -1;
-            result[1] = -1;
-            return result;
-        }
-        result[0] = lb;
-        result[1] = upper_bound(nums,nums.length,target)-1;
-        return result;
+        int left = lower_bound(nums,target);
+        int right = upper_bound(nums,target);
+        return new int[] {left,right}; 
+        
     }
-    public int lower_bound(int A[], int n, int x) {
+    public int lower_bound(int nums[], int target) {
         int left = 0;
-        int right = n-1;
-        int ans = n;
+        int right = nums.length-1;
+        int val = -1;
         while (left<=right) {
             int mid = (left+right)/2;
-            if (A[mid]>=x) {
-                ans = mid;
+            if (nums[mid]==target) {
+                val = mid;
+                right = mid-1;
+            }
+            else if (nums[mid]>target) {
                 right = mid-1;
             }
             else {
                 left = mid+1;
             }
         }
-        return ans;
+        return val;
     }
-    public int upper_bound(int A[], int n, int x) {
+    public int upper_bound(int nums[], int target) {
         int left = 0;
-        int right = n-1;
-        int ans = n;
+        int right = nums.length-1;
+        int val = -1;
         while (left<=right) {
             int mid = (left+right)/2;
-            if (A[mid]>x) {
-                ans = mid;
-                right = mid-1;
-            }
-            else {
+            if (nums[mid]==target) {
+                val = mid;
                 left = mid+1;
             }
+            else if (nums[mid]<target) {
+                left = mid+1;
+            }
+            else {
+                right = mid-1;
+            }
         }
-        return ans;
+        return val;
     }
 }
