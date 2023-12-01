0221602a07f9c3b6e4069721b9013fcb653a8471
BENDU MANOHAR NAIDU <45328696+bendumanoharnaidu2001@users.noreply.github.com>
2023-12-01 12:06:51 +0530

    Create file

---
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -0,0 +1,16 @@
+            if (l2!=null) {
+                sum +=l2.val;
+            }
+                l1 = l1.next;
+                sum += l1.val;
+            if (l1!=null) {
+        while (l1!=null || l2!=null || carry==1) {
+        int carry = 0;
+        ListNode temp = dummy;
+        ListNode dummy = new ListNode();
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+class Solution {
+ */
+ * }
+            int sum = 0;
+[
