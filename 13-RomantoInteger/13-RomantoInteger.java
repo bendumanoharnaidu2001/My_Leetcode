a821132b7e2ca17b56ce480a9480f52283a55567
BENDU MANOHAR NAIDU <45328696+bendumanoharnaidu2001@users.noreply.github.com>
2023-12-01 19:15:52 +0530

    Create file

---
+++ 13-RomantoInteger/13-RomantoInteger.java
@@ -0,0 +1,31 @@
+            }
+            else {
+                ans +=900;
+                i++;
+                i++;
+            }
+            else if (i+1<n && s.charAt(i)=='C' && s.charAt(i+1)=='M') {
+            }
+            else if (i+1<n && s.charAt(i)=='C' && s.charAt(i+1)=='D') {
+                ans +=400;
+                i++;
+                ans +=90;
+            }
+            else if (i+1<n && s.charAt(i)=='X' && s.charAt(i+1)=='C') {
+                i++;
+            else if (i+1<n && s.charAt(i)=='X' && s.charAt(i+1)=='L') {
+                ans +=40;
+                i++;
+            }
+                ans += 9;
+            else if (i+1<n && s.charAt(i)=='I' && s.charAt(i+1)=='X') {
+            }
+                i++;
+                ans += 4;
+            if (i+1<n && s.charAt(i)=='I' && s.charAt(i+1)=='V') {
+        for (int i=0; i<n; i++) {
+        int ans = 0;
+    public int romanToInt(String s) {
+        int n = s.length();
+class Solution {
+"
