f68ad2e93e92f8b34c885e01ade07b8530581e1f
BENDU MANOHAR NAIDU <45328696+bendumanoharnaidu2001@users.noreply.github.com>
2023-12-01 10:40:51 +0530

    Create file

---
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.java
@@ -0,0 +1,18 @@
+        int count = 0;
+        int max = 0;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i]==1) {
+                count++;
+            }
+            else{
+                count=0;
+                max = Math.max(max,count);
+            }
+        }
+        return max;
+    public int findMaxConsecutiveOnes(int[] nums) {
+class Solution {
+    }
+        max = Math.max(max,count);
+}
+[
