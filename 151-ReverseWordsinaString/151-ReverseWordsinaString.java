4aab9d76d1309daa5ca7eeacbbdb657c782b93ab
BENDU MANOHAR NAIDU <45328696+bendumanoharnaidu2001@users.noreply.github.com>
2023-12-02 12:54:38 +0530

    Update file 12/2/2023, 12:54:37 PM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.java
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.java
@@ -1,40 +1,29 @@
 class Solution {
-    public int myAtoi(String s) {
+    public String reverseWords(String s) {
         int n = s.length();
+        String ans = new String();
         int i = 0;
-        int sign = 1;
-        while(i<n && s.charAt(i)==' ') {
-            i++;
-        }
-        if (i>=n) {
-            return 0;
-        }
-        if (s.charAt(i)=='-') {
-            sign = -1;
-        }
-        if (s.charAt(i)=='+' || s.charAt(i)=='-') {
-            i++;
-        }
-
-        if (i>=n) {
-            return 0;
-        }
-        int num = 0;
-        while (i<n && s.charAt(i)>='0' && s.charAt(i)<='9') {
-            int x = s.charAt(i)-'0';
-            //System.out.println(x);
-            if (num>Integer.MAX_VALUE/10 || (num==Integer.MAX_VALUE/10 && x>Integer.MAX_VALUE%10)) {
-                if (sign==1) {
-                    return Integer.MAX_VALUE;
-                }
-                else {
-                    return Integer.MIN_VALUE;
-                }
+        while (i<n) {
+            while (i<n && s.charAt(i)==' ') {
+                i++;
             }
-            num = num*10 + x;
-            i++;
+            if (i>=n) {
+                break;
+            }
+            int j = i+1;
+            while (j<n && s.charAt(j)!=' ') {
+                j++;
+            }
+            String sub = s.substring(i,j);
+            if (ans.length()==0) {
+                ans = sub;
+            }
+            else {
+                ans = sub + " " + ans;
+            }
+            i = j;
         }
-        return num*sign;
+        return ans;
     }
 }
-"42"
+"
