4dd0e73cb294b6359b3963f172a6c08446d08f5d
BENDU MANOHAR NAIDU <45328696+bendumanoharnaidu2001@users.noreply.github.com>
2023-12-04 20:58:29 +0530

    Create file

---
+++ 221-MaximalSquare/221-MaximalSquare.java
@@ -0,0 +1,17 @@
+                ans=1;
+            if(matrix[0][i]=='1'){
+                ds[0][i]=1;
+                ans=1;
+            }
+        }
+        for(int i=0; i<m; i++){
+                ds[i][0]=1;
+            if(matrix[i][0]=='1'){
+        for(int i=0; i<n; i++){
+        int ans = 0;
+        int ds[][] = new int[n][m];
+        int m = matrix[0].length;
+        int n = matrix.length;
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+[
