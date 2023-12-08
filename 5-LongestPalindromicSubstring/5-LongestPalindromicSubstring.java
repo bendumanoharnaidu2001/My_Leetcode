943aa230d2cbe1d73bfeef45973343371532753b
BENDU MANOHAR NAIDU <45328696+bendumanoharnaidu2001@users.noreply.github.com>
2023-12-08 23:13:14 +0530

    Update file 12/8/2023, 11:13:14 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -1,20 +1,28 @@
-        }
-        return s.substring(start,end+1);
-            }
-            if (len > end - start + 1) {
-                start = i- (len - 1) / 2;
-                end = i + len / 2;
-    }
-    public int expand(char[] s, int i, int j) {
-            int len = Math.max(expand(charArray,i,i) , expand(charArray,i,i+1));
-        for (int i=0; i<charArray.length; i++) {
-        int start = 0;
-        int end = 0;
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s.length()==0) {
+            return "";
         }
         char charArray[] = s.toCharArray();
 
-        if (s.length()==0) {
-            return "";
-class Solution {
-    public String longestPalindrome(String s) {
-"
+        int start = 0;
+        int end = 0;
+        for (int i=0; i<charArray.length; i++) {
+            int len = Math.max(expand(charArray,i,i) , expand(charArray,i,i+1));
+            if (len > end - start + 1) {
+                start = i- (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start,end+1);
+    }
+    public int expand(char[] s, int i, int j) {
+        
+        while (i>=0 && j<s.length && s[i]==s[j]) {
+            i--;
+            j++;
+        }
+        return j-i-1;
+    }
+}
+"babad"
