3182315a3a9f0ff9cd32dce9c9a5d4f79f6b5557
BENDU MANOHAR NAIDU <45328696+bendumanoharnaidu2001@users.noreply.github.com>
2023-12-16 21:24:45 +0530

    Update file 12/16/2023, 9:24:45 PM

--- 221-MaximalSquare/221-MaximalSquare.java
+++ 221-MaximalSquare/221-MaximalSquare.java
@@ -1,10 +1,27 @@
-        int ans = 0;
-        int m = matrix[0].length;
-        int ds[][] = new int[n][m];
+class Solution {
     public int maximalSquare(char[][] matrix) {
         int n = matrix.length;
-class Solution {
+        int m = matrix[0].length;
+        int ds[][] = new int[n][m];
+        int ans = 0;
         for (int i=0; i<n; i++) {
             for (int j=0; j<m; j++) {
                 if (i==0 || j==0) {
-[
+                    if (matrix[i][j]=='1') {
+                        ds[i][j] = 1;
+                        ans = Math.max(ans,ds[i][j]);
+                    }
+                }
+                else {
+                    if (matrix[i][j]=='1') {
+                        ds[i][j] = Math.min(ds[i-1][j-1],Math.min(ds[i][j-1],ds[i-1][j]))+1;
+                        ans = Math.max(ans,ds[i][j]);
+                    }
+                }
+            }
+        }
+        return ans*ans;
+    }
+}
+
+[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
