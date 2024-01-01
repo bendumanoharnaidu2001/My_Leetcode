aa281582711cb84d7225b25cb7a29620f840a5a5
BENDU MANOHAR NAIDU <45328696+bendumanoharnaidu2001@users.noreply.github.com>
2024-01-01 20:12:02 +0530

    Create file

---
+++ 455-AssignCookies/455-AssignCookies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+        for (int i=g.length-1, j=s.length-1; i>=0 && j>=0; i--) {
+            if (s[j]>=g[i]) {
+                count++;
+                j--;
+            }
+        }
+        return count;
+    }
+}
+[
